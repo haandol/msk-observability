33c2376a4357c1defcad5454a996d577d45ed3a1
haandol <ldg55d@gmail.com>
2022-09-30 12:14:55 +0900

    fix grammar

--- infra/lib/configs/validators.ts
+++ infra/lib/configs/validators.ts
@@ -2,7 +2,7 @@ import * as joi from 'joi';
 
 export const VpcValidator: joi.CustomValidator = (value: string, helpers) => {
   if (!value.startsWith('vpc-')) {
-    return helpers.error('VPC_ID should be starts with `vpc-`');
+    return helpers.error('VPC_ID should starts with `vpc-`');
   }
   return value;
 };
@@ -12,7 +12,17 @@ export const SubnetValidator: joi.CustomValidator = (
   helpers
 ) => {
   if (!value.startsWith('subnet-')) {
-    return helpers.error('SUBNET_IDS should be starts with `subnet-`');
+    return helpers.error('SUBNET_IDS should starts with `subnet-`');
+  }
+  return value;
+};
+
+export const SecurityGroupValidator: joi.CustomValidator = (
+  value: string,
+  helpers
+) => {
+  if (!value.startsWith('sg-')) {
+    return helpers.error('*SECURITY_GROUP_ID should starts with `sg-`');
   }
   return value;
 };
