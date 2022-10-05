c15bd31c3c7b1de46debadf376afe43595751778
haandol <ldg55d@gmail.com>
2022-10-05 14:39:53 +0900

    use hashcorp providing resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,13 +14,60 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
+resource "aws_security_group" "sg" {
+  name = "msko11y-dev-sg"
+  vpc_id = "vpc-xxx"
+}
+
+resource "aws_msk_cluster" "kafka" {
+  cluster_name           = "msko11y-dev"
+  kafka_version          = "2.8.1"
+  number_of_broker_nodes = 2
+  enhanced_monitoring = "PER_TOPIC_PER_BROKER"
+
+  broker_node_group_info {
+    instance_type = "kafka.m5.large"
+    client_subnets = [
+      "subnet-xxx",
+      "subnet-xxx",
+    ]
+    storage_info {
+      ebs_storage_info {
+        volume_size = 1000
+      }
+    }
+    security_groups = [aws_security_group.sg.id]
+  }
+
+  open_monitoring {
+    prometheus {
+      jmx_exporter {
+        enabled_in_broker = true
+      }
+      node_exporter {
+        enabled_in_broker = true
+      }
+    }
+  }
+}
+
+output "zookeeper_connect_string" {
+  value = aws_msk_cluster.kafka.zookeeper_connect_string
+}
+
+output "bootstrap_brokers_tls" {
+  description = "TLS connection host:port pairs"
+  value = aws_msk_cluster.kafka.bootstrap_brokers_tls
+}
+
+/*
 # https://github.com/cloudposse/terraform-aws-msk-apache-kafka-cluster
 module "kafka" {
   source = "cloudposse/msk-apache-kafka-cluster/aws"
 
   namespace = "pe"
   stage = "test"
-  name = "msko11y"
+  name = "msko11y-dev"
   vpc_id = "REPLACE_WITH_YOUR_VPC_ID"
   subnet_ids = ["REPLACE_WITH_YOUR_SUBNET_ID", "REPLACE_WITH_YOUR_SUBNET_ID"]
   kafka_version = "2.8.1"
@@ -31,3 +78,4 @@ module "kafka" {
   enhanced_monitoring = "PER_TOPIC_PER_BROKER"
   jmx_exporter_enabled = true
 }
+*/
