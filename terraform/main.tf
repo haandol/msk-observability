e24ed1672d00c7da7330db582740a951f8e273c6
haandol <ldg55d@gmail.com>
2022-10-05 15:06:45 +0900

    add egress to sg

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,6 +17,14 @@ provider "aws" {
 resource "aws_security_group" "sg" {
   name = "msko11y-dev-sg"
   vpc_id = "vpc-xxx"
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 }
 
 resource "aws_msk_cluster" "kafka" {
